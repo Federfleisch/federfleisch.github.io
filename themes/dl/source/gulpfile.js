a11e064c249a7ecd0fe72757c908953be830f09c
Maxime Laforet <maxime10081985@gmail.com>
2015-11-02 16:40:38 +0100

    Add some gulp plugins

--- themes/dl/source/gulpfile.js
+++ themes/dl/source/gulpfile.js
@@ -1,28 +1,52 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    cssnext = require("gulp-cssnext");
+    cssnext = require('gulp-cssnext'),
+    uglify = require('gulp-uglify')
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant');
+
 
 gulp.task('styles', function() {
   gulp.src("css/style.css")
   .pipe(cssnext({
     compress: false,
-    import: {
-      path: [
-        "node_modules"
-      ]
-    }
   }))
   .pipe(gulp.dest("./dist/"))
 });
 
+gulp.task('js', function () {
+  gulp.src('js/site.js')
+    .pipe(gulp.dest('./dist/'));
+});
+
+// gulp.task('img', function () {
+//   return gulp.src('/img/**/*')
+//     .pipe(cache(imagemin({
+//       optimizationLevel: 5,
+//       progressive: true,
+//       interlaced: true,
+//       svgoPlugins: [{removeViewBox: false}],
+//       use: [pngquant()]
+//     })))
+//     .pipe(gulp.dest(''));
+// });
+
+
 
 // build
-gulp.task("dist", [
-  "styles",
+gulp.task("dev", [
+  "styles", "js"
 ])
 
-gulp.task("watch", ["dist"], function() {
+// deploy
+// gulp.task("deploy", [
+//   "styles", "js"
+// ])
+
+gulp.task("watch", ["dev"], function() {
+  gulp.watch("js/**/*.js", ["js"])
   gulp.watch("css/**/*.css", ["styles"])
 })
 
-gulp.task("default", ["dist", "watch"])
+gulp.task("default", ["dev", "watch"])
+// gulp.task("prod", ["deploy", "watch"])
---
+++ themes/dl/source/js/site.js
@@ -0,0 +1,53 @@
+var dlWeb = (function() {
+
+  var nav = document.getElementById('dl-Nav');
+  var aside = document.getElementById('dl-Aside');
+  var asideFirstChild = aside.firstChild;
+  var asideLastChild = aside.lastChild;
+  var navItemActive = document.querySelector('.dl-Nav-Item--active');
+  var navFirstChild = nav.firstChild;
+  var isRoot = location.pathname === "/";
+
+  function onResizeOrLoad() {
+    if (isRoot) {
+      return;
+    }
+    if (window.innerWidth < 768) {
+      moveNavUp();
+      moveNavItemActive();
+      openNavOnClickActiveItem();
+    }
+    if (window.innerWidth > 768) {
+      moveNavDown();
+    }
+  }
+
+  function openNavOnClickActiveItem() {
+    navItemActive.addEventListener('click', function(e) {
+      e.preventDefault();
+      nav.classList.toggle('dl-Nav--isOpen');
+    });
+  }
+
+  function moveNavItemActive() {
+    nav.insertBefore(navItemActive, navFirstChild);
+  }
+
+  function moveNavUp() {
+    aside.insertBefore(nav, asideFirstChild.nextSibling.nextSibling);
+  }
+
+  function moveNavDown() {
+    aside.insertBefore(nav, asideLastChild);
+  }
+
+
+  return {
+    setup: function setup() {
+      window.addEventListener("load", onResizeOrLoad);
+      window.addEventListener("resize", onResizeOrLoad);
+    }
+  }
+})();
+
+dlWeb.setup()
